8b931ec9007750631435406f1328c9da74e97075
Cin💜 Lo <cinlos3382@gmail.com>
2023-03-31 17:33:15 -0300

    validacion de token para cambiar contraseña

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -18,16 +18,31 @@ router.post('/login',
   }
 );
 
+
 router.post('/recovery',
   async (req, res, next) => {
     try {
       const { email } = req.body;
-    const rta = await service.sendRecovery(email)
+    const rta = await service.sendRecovery(email);
     res.json(rta);
     } catch (error) {
       next(error);
     }
   }
 );
+
+
+router.post('/change-password',
+  async (req, res, next) => {
+    try {
+      const { token, newPassword } = req.body;
+    const rta = await service.changePassword(token, newPassword);
+    res.json(rta);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 
 module.exports = router;
--- services/auth.services.js
+++ services/auth.services.js
@@ -56,6 +56,20 @@ class AuthServices{
     return rta;
   }
 
+  async changePassword(token, newPassword) {
+    try {
+      const payload = jwt.verify(token, config.jwtSecret);
+      const user = await service.findOne(payload.sub);
+      if(user.recoveryToken !== token) {
+      throw boom.unauthorized();
+      }
+      const hash = await bcrypt.hash(newPassword, 10);
+      await service.update(user.id, { recoveryToken: null, password: hash });
+      return { message: 'password changed' };
+    } catch (error) {
+      throw boom.unauthorized();
+    }
+  }
 
   async sendEmail(infoMail) {
     const transporter = nodemailer.createTransport({
