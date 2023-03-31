6d41be8284f6af04eaf29bb77b4794d453428224
Cin💜 Lo <cinlos3382@gmail.com>
2023-03-31 12:35:41 -0300

    envio de mails con node.js (nodemailer)

--- config/config.js
+++ config/config.js
@@ -7,6 +7,9 @@ const config = {
   dbUrl: process.env.DATABASE_URL,
   apiKey: process.env.API_KEY,
   jwtSecret: process.env.JWT_SECRET,
+  emailSender: process.env.EMAIL_SENDER,
+  emailPassword: process.env.EMAIL_PASSWORD,
+  host: process.env.HOST,
 
 }
 
